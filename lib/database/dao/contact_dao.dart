cae73d2a2dae6a2e03384a0b001570f164986cbc
Edson Garcia <ednosmab@yahoo.com.br>
2022-10-13 16:54:55 -0300

    refatorando metodos save e findAll

--- lib/database/dao/contact_dao.dart
+++ lib/database/dao/contact_dao.dart
@@ -10,24 +10,34 @@ class ContactDao {
 
   Future<int> save(Contact contact) async {
     final Database db = await getDatabase();
+    Map<String, dynamic> contactMap = _toMap(contact);
+    return db.insert('contacts', contactMap);
+  }
+
+  Map<String, dynamic> _toMap(Contact contact) {
     final Map<String, dynamic> contactMap = Map();
     contactMap['name'] = contact.name;
     contactMap['account_number'] = contact.accountNumber;
-    return db.insert('contacts', contactMap);
+    return contactMap;
   }
 
   Future<List<Contact>> findAll() async {
     final Database db = await getDatabase();
     final List<Map<String, dynamic>> result = await db.query('contacts');
-    final List<Contact> contacts = List.empty(growable: true);
-    for (Map<String, dynamic> row in result) {
-      final Contact contact = Contact(
-        row['id'],
-        row['name'],
-        row['account_number'],
-      );
-      contacts.add(contact);
-    }
+    List<Contact> contacts = _toList(result);
     return contacts;
   }
+}
+
+List<Contact> _toList(List<Map<String, dynamic>> result) {
+  final List<Contact> contacts = List.empty(growable: true);
+  for (Map<String, dynamic> row in result) {
+    final Contact contact = Contact(
+      row['id'],
+      row['name'],
+      row['account_number'],
+    );
+    contacts.add(contact);
+  }
+  return contacts;
 }
